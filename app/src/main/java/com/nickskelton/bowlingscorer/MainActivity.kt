1a45f3adf131e7ef1943e33766c1872315415903
Nick Skelton <nick.g.skelton@gmail.com>
2018-05-27 14:26:54 +0200

    initial commit

---
+++ app/src/main/java/com/nickskelton/bowlingscorer/MainActivity.kt
@@ -0,0 +1,89 @@
+package com.nickskelton.bowlingscorer
+
+import android.os.Bundle
+import android.support.design.widget.CoordinatorLayout
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.support.v7.widget.Toolbar
+import android.widget.Button
+import android.widget.EditText
+
+class MainActivity : AppCompatActivity() {
+    private val recyclerView: RecyclerView by lazy {
+        findViewById<RecyclerView>(R.id.recyclerView)
+    }
+    private val submitButton: Button by lazy {
+        findViewById<Button>(R.id.submitButton)
+    }
+    private val editText: EditText by lazy {
+        findViewById<EditText>(R.id.editText)
+    }
+    private val toolbar: Toolbar by lazy {
+        findViewById<Toolbar>(R.id.toolbar)
+    }
+    private val coordinatorLayout: CoordinatorLayout by lazy {
+        findViewById<CoordinatorLayout>(R.id.contentCoordinatorLayout)
+    }
+
+
+    private var rolls = Array(21, { 0 })
+    private var currentRoll: Int = 0
+
+    private val adapter = FrameAdapter(this)
+    private var scoreCard = Scorecard(rolls)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+        submitButton.setOnClickListener {
+            onRoll()
+        }
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = adapter
+    }
+
+    private fun onRoll() {
+        val validRoll: Int? = validateInput()
+        validRoll?.let {
+
+            //finished game
+            if (currentRoll >= rolls.size) {
+                reset()
+            }
+
+            rolls[currentRoll] = it
+            val newScorecard = Scorecard(rolls)
+            if (!newScorecard.isValid) {
+                Snackbar.make(coordinatorLayout, "Invalid entry", Snackbar.LENGTH_LONG).show()
+                return
+            }
+
+            adapter.update(newScorecard.frames)
+            scoreCard = newScorecard
+            currentRoll++
+        }
+    }
+
+    private fun reset() {
+        rolls = Array(21, { 0 })
+        currentRoll = 0
+    }
+
+    private fun validateInput(): Int? {
+        try {
+            val roll = editText.text.toString().toInt()
+            if (roll in 1..10) {
+                return roll
+            }
+        } catch (e: NumberFormatException) {
+
+        }
+        Snackbar.make(coordinatorLayout, "Invalid entry", Snackbar.LENGTH_LONG).show()
+        editText.setText("")
+        return null
+    }
+}
---
+++ app/src/test/java/com/nickskelton/bowlingscorer/FrameTests.kt
@@ -0,0 +1,126 @@
+package com.nickskelton.bowlingscorer
+
+import junit.framework.Assert.assertFalse
+import junit.framework.Assert.assertTrue
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class FrameTests {
+    @Test
+    fun strike() {
+        //Given
+        val strike = Strike(3, 5)
+        //When
+
+        //Then
+        assertEquals(18, strike.frameValue)
+    }
+
+    @Test
+    fun spare() {
+        //Given
+        val spare = Spare(4, 6)
+
+        //When
+
+        //Then
+        assertEquals(16, spare.frameValue)
+    }
+
+    @Test
+    fun normalFrame() {
+        //Given
+        val frame = NormalFrame(2, 1)
+
+        //When
+
+        //Then
+        assertEquals(3, frame.frameValue)
+    }
+    @Test
+    fun invalidNormalFrame() {
+        //Given
+        val frame = NormalFrame(2, 9)
+
+        //When
+
+        //Then
+        assertFalse(frame.valid)
+    }
+
+    @Test
+    fun invalidEndFrame_extra() {
+        //Given
+        val frame = EndFrame(2, 1, 3)
+
+        //When
+
+        //Then
+        assertFalse(frame.valid)
+    }
+
+    @Test
+    fun invalidEndFrame_start() {
+        //Given
+        val frame = EndFrame(9, 9, 3)
+
+        //When
+
+        //Then
+        assertFalse(frame.valid)
+    }
+    @Test
+    fun invalidEndFrame_end() {
+        //Given
+        val frame = EndFrame(10, 9, 3)
+
+        //When
+
+        //Then
+        assertFalse(frame.valid)
+    }
+
+    @Test
+    fun validEndFrame() {
+        //Given
+        val frame = EndFrame(10, 10, 10)
+
+        //When
+
+        //Then
+        assertTrue(frame.valid)
+    }
+
+    @Test
+    fun validEndFrame_endNothing() {
+        //Given
+        val frame = EndFrame(10, 0, 0)
+
+        //When
+
+        //Then
+        assertTrue(frame.valid)
+    }
+
+    @Test
+    fun validEndFrame_endSpare() {
+        //Given
+        val frame = EndFrame(10, 9, 1)
+
+        //When
+
+        //Then
+        assertTrue(frame.valid)
+    }
+
+    @Test
+    fun validEndFrame_SpareStrike() {
+        //Given
+        val frame = EndFrame(2, 8, 10)
+
+        //When
+
+        //Then
+        assertTrue(frame.valid)
+    }
+}
